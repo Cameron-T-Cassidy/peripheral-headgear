a9579a5689fa6cddf87074e81eebea9e85b5dd02
rossmichaelyoung <rossmichaelyoung@gmail.com>
2020-03-03 12:53:03 -0600

    can control two motors at once

--- output/control_two_motors.py
+++ output/control_two_motors.py
@@ -2,54 +2,54 @@ import RPi.GPIO as GPIO
 import time
 import threading
 
-left_channel = 10
-right_channel = 12
+left_channel = 8
+right_channel = 16
 
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(left_channel, GPIO.OUT)
 GPIO.setup(right_channel, GPIO.OUT)
 
 def on(channel):
-	GPIO.output(channel, GPIO.HIGH)
-	
+    GPIO.output(channel, GPIO.HIGH)
+    
 def off(channel):
-	GPIO.output(channel, GPIO.LOW)
-	
+    GPIO.output(channel, GPIO.LOW)
+    
 def cycle(channel, num_cycles):
-	for i in range(0, num_cycles):
-		on(channel)
-		time.sleep(0.75)
-		off(channel)
-		time.sleep(0.25)
+    for i in range(0, num_cycles):
+        on(channel)
+        time.sleep(0.75)
+        off(channel)
+        time.sleep(0.25)
 
-# print_cycle prints out which motor would be called if we had two motors		
+# print_cycle prints out which motor would be actived but does not activate it      
 def print_cycle(channel, num_cycles):
-	for i in range(0, num_cycles):
-		print(channel)
-		
+    for i in range(0, num_cycles):
+        print(channel)
+        
 def alert(direction, num_cycles):
-	if direction == "left":
-		cycle(left_channel, num_cycles)
-	elif direction == "right":
-		cycle(right_channel, num_cycles)
-	elif direction == "center":
-		t1 = threading.Thread(target=print_cycle, args=(left_channel, num_cycles,))
-		t2 = threading.Thread(target=print_cycle, args=(right_channel, num_cycles,))
-		t1.start()
-		t2.start()
-		t1.join()
-		t2.join()
-	else:
-		print("Direction not valid")
+    if direction == "left":
+        cycle(left_channel, num_cycles)
+    elif direction == "right":
+        cycle(right_channel, num_cycles)
+    elif direction == "center":
+        t1 = threading.Thread(target=cycle, args=(left_channel, num_cycles,))
+        t2 = threading.Thread(target=cycle, args=(right_channel, num_cycles,))
+        t1.start()
+        t2.start()
+        t1.join()
+        t2.join()
+    else:
+        print("Direction not valid")
 
-if __name__ == "__main__":		
-	try:
-		while True:
-			direction = raw_input("Enter direction of input: ")
-			num_cycles = input("Enter number of cycles: ")
-			alert(direction, num_cycles)
-			
-	except KeyboardInterrupt:
-		print("Ctrl-C Pressed: Exiting Program")
+if __name__ == "__main__":      
+    try:
+        while True:
+            direction = raw_input("Enter direction of input: ")
+            num_cycles = input("Enter number of cycles: ")
+            alert(direction, num_cycles)
+            
+    except KeyboardInterrupt:
+        print("Ctrl-C Pressed: Exiting Program")
 
-	GPIO.cleanup()
+    GPIO.cleanup()
